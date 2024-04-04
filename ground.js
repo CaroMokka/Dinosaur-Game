29d41bacc381eab57dce7eb18bfc8a94cdf9d0fa
carolina <ci.arayagonzalez@gmail.com>
2024-04-03 22:35:38 -0300

    :sparkles: Update ground move

--- ground.js
+++ ground.js
@@ -1,13 +1,20 @@
-import { incrementCustomProperty } from "./updateCustomProperty.js";
+import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./updateCustomProperty.js";
 
 const SPEED = .05
-const groundElemts = document.querySelector('[data-ground]');
-//console.log(groundElemts);
+const groundElemts = document.querySelectorAll("[data-ground]");
+console.log(groundElemts);
 
+export function setupGround() {
+    setCustomProperty(groundElemts[0], "--left", 0);
+    setCustomProperty(groundElemts[1], "--left", 300);
+}
 
-
-export function updateGround(delta) {
+export function updateGround(delta, speedScale) {
     groundElemts.forEach(ground => {
-        incrementCustomProperty(ground, "--left", delta * SPEED * -1)
+        incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1)
+
+        if(getCustomProperty(ground, "--left") <= -300){
+            incrementCustomProperty(ground, "--left", 600);
+        }
     })
 }
--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-import { updateGround } from "./ground";
+import { setupGround, updateGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
 
 setPixelToWorldScale();
 document.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, {once: true});
+
+setupGround();
 
 let lastTime;
+let speedScale;
+let score;
 function update(time) {
     if(lastTime == null){
         lastTime = time;
@@ -18,12 +25,31 @@ function update(time) {
   const delta = time - lastTime;
   //console.log(delta);
 
-  updateGround(delta);
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
-window.requestAnimationFrame(update);
+
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta){
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  window.requestAnimationFrame(update);
+}
+
 function setPixelToWorldScale() {
   let worldToPixelScale;
   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
